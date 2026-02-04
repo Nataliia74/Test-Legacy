c84e5b5018d6ff155aba431a73ef277e4e2ab5ad
Nataliia Volkova <volkov.nataliia@gmail.com>
2026-02-04 12:47:33 +0000

    added unfollow logic

--- front-end/components/profile.mjs
+++ front-end/components/profile.mjs
@@ -1,4 +1,4 @@
-import {apiService} from "../index.mjs";
+import { apiService } from "../index.mjs";
 
 /**
  * Create a profile component
@@ -6,7 +6,7 @@ import {apiService} from "../index.mjs";
  * @param {Object} profileData - The profile data to display
  * @returns {DocumentFragment} - The profile UI
  */
-function createProfile(template, {profileData, whoToFollow, isLoggedIn}) {
+function createProfile(template, { profileData, whoToFollow, isLoggedIn }) {
   if (!template || !profileData) return;
   const profileElement = document
     .getElementById(template)
@@ -15,11 +15,18 @@ function createProfile(template, {profileData, whoToFollow, isLoggedIn}) {
   const usernameEl = profileElement.querySelector("[data-username]");
   const bloomCountEl = profileElement.querySelector("[data-bloom-count]");
   const followingCountEl = profileElement.querySelector(
-    "[data-following-count]"
+    "[data-following-count]",
   );
   const followerCountEl = profileElement.querySelector("[data-follower-count]");
   const followButtonEl = profileElement.querySelector("[data-action='follow']");
-  const whoToFollowContainer = profileElement.querySelector(".profile__who-to-follow");
+  const unfollowButtonEl = profileElement.querySelector(
+    "[data-action='unfollow']",
+  );
+  // console.log("follow btn", followButtonEl);
+  // console.log("unfollow btn", unfollowButtonEl);
+  const whoToFollowContainer = profileElement.querySelector(
+    ".profile__who-to-follow",
+  );
   // Populate with data
   usernameEl.querySelector("h2").textContent = profileData.username || "";
   usernameEl.setAttribute("href", `/profile/${profileData.username}`);
@@ -32,9 +39,17 @@ function createProfile(template, {profileData, whoToFollow, isLoggedIn}) {
   if (!isLoggedIn) {
     followButtonEl.style.display = "none";
   }
+  unfollowButtonEl.setAttribute("data-username", profileData.username || "");
+  unfollowButtonEl.hidden = profileData.is_self || !profileData.is_following;
+  unfollowButtonEl.addEventListener("click", handleUnfollow);
+  if (!isLoggedIn) {
+    followButtonEl.style.display = "none";
+  }
 
   if (whoToFollow.length > 0) {
-    const whoToFollowList = whoToFollowContainer.querySelector("[data-who-to-follow]");
+    const whoToFollowList = whoToFollowContainer.querySelector(
+      "[data-who-to-follow]",
+    );
     const whoToFollowTemplate = document.querySelector("#who-to-follow-chip");
     for (const userToFollow of whoToFollow) {
       const wtfElement = whoToFollowTemplate.content.cloneNode(true);
@@ -63,7 +78,18 @@ async function handleFollow(event) {
   if (!username) return;
 
   await apiService.followUser(username);
+  // await apiService.getProfile(username);
   await apiService.getWhoToFollow();
 }
 
-export {createProfile, handleFollow};
+async function handleUnfollow(event) {
+  const button = event.target;
+  const username = button.getAttribute("data-username");
+  if (!username) return;
+
+  await apiService.unfollowUser(username);
+  // await apiService.getProfile(username);
+  await apiService.getWhoToFollow();
+}
+
+export { createProfile, handleFollow, handleUnfollow };
